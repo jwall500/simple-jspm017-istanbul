1bc4ee54c1edcf70a57f966ac28e22f82bac2262
Jessica Wall <jwall@webassign.net>
2016-09-09 13:01:32 -0400

    Add ES6 to main.js. Move create-coverage-bundle.js

--- create-coverage-bundle.js
+++ create-coverage-bundle.js
@@ -5,10 +5,9 @@ var path = require('path');
 var Builder = require("jspm").Builder;
 var builder = new Builder();
 var systemIstanbul = require('systemjs-istanbul-hook');
-var parseArgs = require('minimist');
 
 systemIstanbul.hookSystemJS(builder.loader);
-builder.bundle('lib/main', path.join(__dirname, '../lib/main-bundle-systemIstanbul.js')).then(function() {
+builder.bundle('lib/main', path.join(__dirname, 'lib/main-bundle-systemIstanbul.js')).then(function() {
     var originalSources = systemIstanbul.originalSources;
     fs.writeFileSync('original-source-data.json', JSON.stringify(originalSources));
 });
